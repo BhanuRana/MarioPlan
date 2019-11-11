d42089a329811af627d928cc9b93f05aa4518818
BhanuRana <bhanu2714@gmail.com>
2019-11-11 20:02:46 +0530

    add ProjectDetails component

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ProjectDetails = (props) => {
+	const id = props.match.params.id;
+	return (
+		<div className="container section project-details">
+			<div className="card z-depth-0">
+				<div className="card-content">
+					<span className="card-title">Project Title - {id}</span>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio at rem optio voluptatibus
+						eum sed vitae consequatur facilis mollitia maiores beatae, odio unde libero cum impedit suscipit
+						ratione. Nihil, ratione.
+					</p>
+				</div>
+				<div className="card-action grey lighten-4 grey-text">
+					<div>Posted by The Net Ninja</div>
+					<div>2nd September, 2am</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ProjectDetails;
