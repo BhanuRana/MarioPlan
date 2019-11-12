a7b39ce66a7af32c791562b8a66b44c17bbfb3da
BhanuRana <bhanu2714@gmail.com>
2019-11-12 18:13:53 +0530

    User signOut + conditional show of links in navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,22 +4,24 @@ import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = (props) => {
+	const { auth } = props;
 	return (
 		<nav className="nav-wrapper grey darken-3">
 			<div className="container">
 				<Link to="/" className="brand-logo">
 					MarioPlan
 				</Link>
-				<SignedInLinks />
-				<SignedOutLinks />
+				{auth.uid ? <SignedInLinks /> : <SignedOutLinks />}
 			</div>
 		</nav>
 	);
 };
 
 const mapStateToProps = (state) => {
-	return {};
+	return {
+		auth: state.firebase.auth
+	};
 };
 
 export default connect(mapStateToProps)(Navbar);
